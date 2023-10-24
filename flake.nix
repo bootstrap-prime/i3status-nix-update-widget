f19fe5f99538370c41bbb79a87a4fdcce08659aa
bootstrap-prime <bootstrap.prime@gmail.com>
2023-10-24 03:00:54 -0700

    feat: improved operating principle
    now retrieves date from the statically compiled flake.lock (overridable)
    so that it will be updated in lock step with the generation.

--- flake.nix
+++ flake.nix
@@ -23,81 +23,149 @@
         pkgs = import nixpkgs { inherit system overlays; };
 
         rust-custom-toolchain = (pkgs.rust-bin.stable.latest.default.override {
-              extensions = [
-                "rust-src"
-                "rustfmt"
-                "llvm-tools-preview"
-                "rust-analyzer-preview"
-              ];
+          extensions = [ "rust-src" ];
         });
 
-      in rec {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-          ];
+        craneLib =
+          (inputs.crane.mkLib pkgs).overrideToolchain rust-custom-toolchain;
 
-          nativeBuildInputs = with pkgs; [
-            # get current rust toolchain defaults (this includes clippy and rustfmt)
-            rust-custom-toolchain
+      in {
+        # devShell = pkgs.mkShell {
+        #   buildInputs = with pkgs; [ ];
 
-            cargo-edit
-          ];
+        #   nativeBuildInputs = with pkgs; [
+        #     # get current rust toolchain defaults (this includes clippy and rustfmt)
+        #     rust-custom-toolchain
 
-          # fetch with cli instead of native
-          CARGO_NET_GIT_FETCH_WITH_CLI = "true";
-          RUST_BACKTRACE = 1;
-        };
+        #     cargo-edit
+        #   ];
 
-        default = {};
+        #   # fetch with cli instead of native
+        #   CARGO_NET_GIT_FETCH_WITH_CLI = "true";
+        #   RUST_BACKTRACE = 1;
+        # };
 
-        checks = let
-          craneLib =
-            (inputs.crane.mkLib pkgs).overrideToolchain rust-custom-toolchain;
-          src = ./.;
+        packages.default = (self.packages.${system}.defaultthing { });
 
-          cargoArtifacts = craneLib.buildDepsOnly {
-            inherit src;
-            buildInputs = with pkgs; [ openssl pkg-config ];
-          };
-          build-tests = craneLib.buildPackage {
-            inherit cargoArtifacts src;
-            buildInputs = with pkgs; [ openssl pkg-config capnproto ];
-          };
-        in {
-          inherit build-tests;
+        packages.defaultthing =
+          { flakelock ? ./flake.lock, threshold ? 14, icon ? "cogs", ... }:
+          with pkgs;
+          let
+            # flakelock = ./flake.lock;
+            # threshold = 14;
+            # icon = "cogs";
+            lockfile = builtins.fromJSON (builtins.readFile flakelock);
+            recenttime = builtins.head (lib.sort (a: b: a > b)
+              (map (key: lockfile.nodes.${key}.locked.lastModified or 0)
+                (lib.attrNames lockfile.nodes)));
 
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, resuing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          my-crate-clippy = craneLib.cargoClippy {
-            inherit cargoArtifacts src;
-            cargoClippyExtraArgs = "-- --deny warnings";
+            config_file = pkgs.writeText "modified_data.rs" ''
+              const MODIFIED_DATE: i64 = ${toString recenttime};
+              const GOOD_THRESHOLD: i64 = 3;
+              const UPDATE_THRESHOLD: i64 = 4;
+              const OUT_OF_DATE_THRESHOLD: i64 = ${toString threshold};
+              const STATUS_ICON: &str = "${icon}";
+            '';
 
-            buildInputs = with pkgs; [ openssl pkg-config capnproto ];
-          };
-
-          # Check formatting
-          my-crate-fmt = craneLib.cargoFmt { inherit src; };
-
-          # Audit dependencies
-          my-crate-audit = craneLib.cargoAudit {
-            inherit src;
-            advisory-db = inputs.advisory-db;
-            cargoAuditExtraArgs = "--ignore RUSTSEC-2020-0071";
+            cargoArtifacts = craneLib.buildDepsOnly {
+              src = ./.;
+              buildInputs = [ ];
+            };
+          in craneLib.buildPackage {
+            inherit cargoArtifacts;
+            src = ./.;
+            buildInputs = [ ];
+            patchPhase = ''
+              cp ${config_file} $src/modified_data.rs
+            '';
           };
 
-          # Run tests with cargo-nextest
-          my-crate-nextest = craneLib.cargoNextest {
-            inherit cargoArtifacts src;
-            partitions = 1;
-            partitionType = "count";
+        # nixosModule = { config, lib, pkgs, ... }:
+        #   with lib;
+        #   let cfg = config.programs.updatewidget;
+        #   in {
+        #     options.programs.updatewidget = {
+        #       enable = mkEnableOption "Enables the nix flake update widget";
 
-            buildInputs = with pkgs; [ openssl pkg-config capnproto ];
-          };
-        };
+        #       flakelock = mkOption {
+        #         type = types.str;
+        #         example = "./flake.lock";
+        #         description =
+        #           "the location of the flake lock to get a bearing on";
+        #       };
+
+        #       icon = mkOption {
+        #         type = types.str;
+        #         default = "cogs";
+        #         description = "the i3status icon to place next to the age.";
+        #       };
+
+        #       threshold = mkOption {
+        #         type = types.int;
+        #         example = 3;
+        #         description =
+        #           "the number of days after which the flake is out of date";
+        #       };
+        #     };
+
+        #     config = let
+        #       # read in flake.lock from location
+        #       # iterate through and find the most recent modified date in all inputs
+        #       # the user is expected to not update only specific entries in the flake so
+        #       # we can just take the most recent thing as an indication of when the flake was last updated
+        #       # and we're going to ignore that sometimes flakes just don't receive updates because nixpkgs is being constantly updated
+
+        #     in mkIf cfg.enable {
+
+        #     };
+        #   };
+
+        # checks = let
+        #   src = ./.;
+
+        #   cargoArtifacts = craneLib.buildDepsOnly {
+        #     inherit src;
+        #     buildInputs = with pkgs; [ openssl pkg-config ];
+        #   };
+        #   build-tests = craneLib.buildPackage {
+        #     inherit cargoArtifacts src;
+        #     buildInputs = with pkgs; [ openssl pkg-config capnproto ];
+        #   };
+        # in {
+        #   inherit build-tests;
+
+        #   # Run clippy (and deny all warnings) on the crate source,
+        #   # again, resuing the dependency artifacts from above.
+        #   #
+        #   # Note that this is done as a separate derivation so that
+        #   # we can block the CI if there are issues here, but not
+        #   # prevent downstream consumers from building our crate by itself.
+        #   my-crate-clippy = craneLib.cargoClippy {
+        #     inherit cargoArtifacts src;
+        #     cargoClippyExtraArgs = "-- --deny warnings";
+
+        #     buildInputs = with pkgs; [ openssl pkg-config capnproto ];
+        #   };
+
+        #   # Check formatting
+        #   my-crate-fmt = craneLib.cargoFmt { inherit src; };
+
+        #   # Audit dependencies
+        #   my-crate-audit = craneLib.cargoAudit {
+        #     inherit src;
+        #     advisory-db = inputs.advisory-db;
+        #     cargoAuditExtraArgs = "--ignore RUSTSEC-2020-0071";
+        #   };
+
+        #   # Run tests with cargo-nextest
+        #   my-crate-nextest = craneLib.cargoNextest {
+        #     inherit cargoArtifacts src;
+        #     partitions = 1;
+        #     partitionType = "count";
+
+        #     buildInputs = with pkgs; [ openssl pkg-config capnproto ];
+        #   };
+        # };
 
       });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,62 @@
 // this COULD run flake update and compare dates, BUT I don't want to
 // because then I would have to figure out how to check less often and consume less compute
 
-#[derive(serde::Deserialize, Clone, Copy)]
-struct Configuration {
-    lock_path: String,
-    inputs: Vec<String>,
-    threshold: i32,
-    // you COULD run a command every time you update. however, this would be somewhat wasteful because this thing is going to be running constantly
-    // command: Option<String>,
+// simplest solution: when the system gets rebuilt it takes information from the flake.lock and
+// commits it. the module for this will take that info and put it in here to include it as a constant.
+
+include!("../modified_date.rs");
+
+#[derive(serde::Serialize)]
+pub enum State {
+    Info,
+    Good,
+    Warning,
+    Critical,
 }
 
-fn main() {
-    // wtf does tauri do here
-
-    // be constantly checking if you're up to date by deserializing the flake lockfile and checking if you're many whole days away from the last update
-    // update the taskbar icon
-
-    println!("Hello, world!");
+#[derive(serde::Serialize)]
+pub struct BarCommand {
+    icon: String,
+    state: State,
+    text: String,
 }
 
-// notifcation taskbar icon stuff, check every day at a certain time?
+use anyhow::Context;
+use chrono;
 
-// get config information, which inputs you care about, how many days to warn the user, update command
-fn get_config() -> anyhow::Result<Configuration> {
-    // get XDG config path
-    // get file
-    // toml deserialize file into Configuration
+fn main() -> anyhow::Result<()> {
+    let now = chrono::Utc::now();
 
-    Ok(config)
-}
+    let time = chrono::DateTime::from_timestamp(MODIFIED_DATE, 0)
+        .context("Could not deserialize timestamp. Corrupted flake?")?;
 
-// if older than this, be alarmed
-const DAY_THRESHOLD: i32 = 3;
-const HIGHLIGHTED_INPUT: &str = "nixpkgs";
+    let duration_days = now.signed_duration_since(time).num_days();
 
-// ❯ date -d @(bat testlock.lock | jq '.nodes[.nodes.root.inputs.master].locked.lastModified')
-fn flake_last_updated() -> anyhow::Result<()> {
-    // read in flake.lock from location
-    // deserialize into a struct (what is the format?)
+    let status: State;
 
-    // iterate through and find the most recent modified date in all inputs
-    // the user is expected to not update only specific entries in the flake so
-    // we can just take the most recent thing as an indication of when the flake was last updated
+    if duration_days >= OUT_OF_DATE_THRESHOLD {
+        // it is critical that you update
+        status = State::Critical;
+    } else if duration_days >= UPDATE_THRESHOLD {
+        // warn to update
+        status = State::Warning;
+    } else if duration_days <= GOOD_THRESHOLD {
+        // you don't need to update yet
+        status = State::Good;
+    } else {
+        unreachable!("all possible values of duration_days are handled");
+    }
+
+    let code = BarCommand {
+        icon: STATUS_ICON.to_string(),
+        state: status,
+        text: format!("Age: {}", duration_days),
+    };
+
+    println!(
+        "{}",
+        serde_json::to_string(&code).context("Could not serialize status")?
+    );
 
     Ok(())
 }
